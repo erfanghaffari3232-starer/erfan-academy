0ede96f77b09b88ad32b74c23e7d3b7f58d35561
erfanghaffari3232-starer <erfanghaffari3232@gmail.com>
2026-08-20 17:56:19 +0330

    Remove whiteboard files

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$room = isset($_GET['room']) ? preg_replace('/[^a-zA-Z0-9_-]/', '', $_GET['room']) : 'main';
-if ($room === '') $room = 'main';
-header('Location: board.php?room=' . urlencode($room));
-exit;
-?>
